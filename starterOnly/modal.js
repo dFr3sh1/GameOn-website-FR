cf5d2a55c6c2995d07ca85819a609bb36a8d18df
DFr3sh <dfr3sh1@protonmail.com>
2024-01-31 11:38:04 +0100

    Rename function validateMinLength for validateNameLength in modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -89,8 +89,8 @@ function resetErrorState(element) {
 
 // Validation rules for each form element
 const validationRules = {
-  first: (element) => validateMinLength(element, 2, 'Le prénom est requis et doit comporté au moins deux caractères.'),
-  last: (element) => validateMinLength(element, 2, 'Le nom est requis et doit comporté au moins deux caractères.'),
+  first: (element) => validateNameLength(element, 2, 'Le prénom est requis et doit comporté au moins deux caractères.'),
+  last: (element) => validateNameLength(element, 2, 'Le nom est requis et doit comporté au moins deux caractères.'),
   email: (element) => validateEmail(element, 'Veuillez saisir une adresse e-mail valide.'),
   birthdate: (element) => validateRequired(element, 'La date de naissance est requise.'),
   quantity: (element) => validateNumberRange(element, 0, 99, 'Veuillez entrer un nombre valide.'),
@@ -107,7 +107,7 @@ function validateRequired(element, errorMessage) {
   return true;
 }
 
-function validateMinLength(element, minLength, errorMessage) {
+function validateNameLength(element, minLength, errorMessage) {
   if (element.value.trim().length < minLength) {
     setValidationError(element, errorMessage);
     return false;
